eaca1a5ee5919498fc474e022b93638efc70f435
JaredVS777 <jared.valenzuelajvs02@gmail.com>
2024-06-10 12:12:26 -0500

    Deploy en la web

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig :{
+    apiKey: "AIzaSyDbfLMgrTWjMh5G0nANvA4ywQb_smzMzBk",
+    authDomain: "fotos1-1f12b.firebaseapp.com",
+    projectId: "fotos1-1f12b",
+    storageBucket: "fotos1-1f12b.appspot.com",
+    messagingSenderId: "911656884055",
+    appId: "1:911656884055:web:88a185597dd1e9a2379012"
+  }
 };
