27a18db3d5cf904009c45880915ca7d1fe47fb94
JaredVS777 <jared.valenzuelajvs02@gmail.com>
2024-06-10 15:01:58 -0500

    Variables de entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig :{
-    apiKey: "AIzaSyDbfLMgrTWjMh5G0nANvA4ywQb_smzMzBk",
-    authDomain: "fotos1-1f12b.firebaseapp.com",
-    projectId: "fotos1-1f12b",
-    storageBucket: "fotos1-1f12b.appspot.com",
-    messagingSenderId: "911656884055",
-    appId: "1:911656884055:web:88a185597dd1e9a2379012"
+  
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig :{
-    apiKey: "AIzaSyDbfLMgrTWjMh5G0nANvA4ywQb_smzMzBk",
-    authDomain: "fotos1-1f12b.firebaseapp.com",
-    projectId: "fotos1-1f12b",
-    storageBucket: "fotos1-1f12b.appspot.com",
-    messagingSenderId: "911656884055",
-    appId: "1:911656884055:web:88a185597dd1e9a2379012"
+    
   }
 };
 
